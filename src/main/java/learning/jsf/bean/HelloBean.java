6d5871c6ecef1ff25897740be75539dd9e1f62ad
unknown <mohammed.ali@IHE-MAliDEV.ihorizons.com>
2014-12-15 17:00:52 +0200

    first commit

---
+++ src/main/java/learning/jsf/bean/HelloBean.java
@@ -0,0 +1,24 @@
+package learning.jsf.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloBean implements Serializable {
+ 
+	private static final long serialVersionUID = 1L;
+ 
+	private String name;
+ 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
